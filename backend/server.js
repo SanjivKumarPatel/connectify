ab5f4c9e4bf5ecfba8f40439f0f1ed87c232534d
SanjivKumarPatel <sanjivpatel00001@gmail.com>
2026-05-22 00:41:44 +0530

    initialize frontend and backend with Tailwind and Express setup

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import dns from 'dns'
+
+dns.setDefaultResultOrder('ipv4first')
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Connectify API Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
