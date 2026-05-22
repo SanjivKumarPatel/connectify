fa810b4ef208aa2909aaab5dfc10b3a36273c24c
SanjivKumarPatel <sanjivpatel00001@gmail.com>
2026-05-22 23:13:59 +0530

    Complete register API

---
+++ backend/controllers/authController.js
@@ -0,0 +1,85 @@
+import User from '../models/User.js'
+import generateToken from '../utils/generateToken.js'
+import bcrypt from 'bcryptjs'
+
+// Register User
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body
+
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: 'All fields are required'
+      })
+    }
+
+    const existingUser = await User.findOne({ email })
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: 'User already exists'
+      })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    })
+
+    res.status(201).json({
+      success: true,
+      token: generateToken(user._id),
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email
+      }
+    })
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    })
+  }
+}
+
+// Login User
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      return res.status(401).json({
+        message: 'Invalid email or password'
+      })
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+      return res.status(401).json({
+        message: 'Invalid email or password'
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      token: generateToken(user._id),
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email
+      }
+    })
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    })
+  }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import connectDB from "./config/db.js";
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import router from './routes/authRoutes.js'
 
 dotenv.config();
 
@@ -12,7 +13,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.send("backend running properly")
+})
+app.use('/api/auth', router)
 
 //global error hander
 app.use((err, req, res, next) => {
