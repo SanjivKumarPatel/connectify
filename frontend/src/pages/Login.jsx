7ccc0df14eb97608d593ff4e190e7d54a778d810
SanjivKumarPatel <sanjivpatel00001@gmail.com>
2026-05-27 22:54:51 +0530

    Prepare project for deployment

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -74,7 +74,7 @@ const Login = () => {
       <div className='sm:mx-auto sm:w-full sm:max-w-md'>
         <div className='text-center'>
           <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
-            Welcome Back, to KodeMedia
+            Welcome Back, to Connectify
           </h2>
           <p className='mt-2 text-center text-sm text-gray-600'>
             Log in to your account to start posting
